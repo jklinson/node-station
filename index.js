bc3079d57a1818d60b01abe835b02317cb996e23
Linson <linson@azurethailand.com>
2020-06-02 15:40:14 +0700

    read file reader utility in index js

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 const Logger = require('./utilities/Logger');
+const FileReader = require('./utilities/FileReader');
 
 Logger.info(process.argv);
 
 var fileName = process.argv[2]? process.argv[2].split('=')[1]: '';
 
 if (fileName.length > 0) {
-    //processFile();
+    FileReader.readFile(fileName).then((data)=> {
+        Logger.info(data);
+    })
+    .catch((err) => {
+        Logger.error(err);
+    });
 } else {
     Logger.error(`You have not included CSV file name with the run command.
      please include it like given below. \n
-     ndoe .\station.js --file=[Your file name]`);
+     ndoe .\index.js --file=[Your file name]`);
 }
 
 
