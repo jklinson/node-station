552bef97e9b02b394d00891d64762cfa7f0ec1f6
Linson Alfred <linson@Linsons-MacBook-Pro.local>
2020-06-03 15:59:20 +0700

    added implementation of csv parser and file reader

--- index.js
+++ index.js
@@ -1,21 +1,46 @@
 const Logger = require('./utilities/Logger');
 const FileReader = require('./utilities/FileReader');
+const CSVParser = require('./utilities/CSVParser');
 
 Logger.info(process.argv);
 
 var fileName = process.argv[2]? process.argv[2].split('=')[1]: '';
 
 if (fileName.length > 0) {
-    FileReader.readFile(fileName).then((data)=> {
-        Logger.info(data);
-    })
-    .catch((err) => {
-        Logger.error(err);
-    });
+    try {
+        FileReader.readFile(fileName).then((data)=> {
+            let parser = new CSVParser(data);
+            let stationDetails = parser.getTableObject();
+            Logger.info(stationDetails);
+            let routes = {source: {}, destination: {}};
+            stationDetails.forEach((item, index) => {
+                let sourceStation = {};
+                if (routes.source[item.column_1]) {
+                    sourceStation = routes.source[item.column_1];                    
+                }
+                sourceStation[item.column_2] = item.column_3;
+                routes.source[item.column_1] = sourceStation;
+
+                let destStation = [];
+                if (routes.destination[item.column_2]) {
+                    destStation = routes.destination[item.column_2];                    
+                }
+                destStation.push(item.column_1);
+                routes.destination[item.column_2] = destStation;
+
+            });
+            Logger.info(routes);
+        })
+        .catch((err) => {
+            Logger.error(err);
+        });
+    } catch (error) {
+        Logger.error(error);
+    }
 } else {
     Logger.error(`You have not included CSV file name with the run command.
      please include it like given below. \n
-     ndoe .\index.js --file=[Your file name]`);
+     node .\index.js --file=[Your file name]`);
 }
 
 
