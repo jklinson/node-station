11fc5ae64e4075e65c5c10e0317af5a6241eaec5
Linson Alfred <linson@Linsons-MacBook-Pro.local>
2020-06-03 15:56:05 +0700

    corrected logger

--- utilities/Logger.js
+++ utilities/Logger.js
@@ -1,12 +1,12 @@
 var Logger = {
     info: function (info) { 
-        console.info('Info: ' + info);
+        console.info('Info: ', info);
     },
     warning:function (warning) { 
-        console.warn('Warning: ' + warning);
+        console.warn('Warning: ', warning);
     },
     error:function (error) { 
-        console.error('Error: ' + error);
+        console.error('Error: ', error);
     }
 };
 
